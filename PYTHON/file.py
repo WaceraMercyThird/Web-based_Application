f9e121d3cd8cee94af2e7bd3f148428caf4ab4cf
WaceraMercyThird <waceraaamercy@gmail.com>
2022-03-29 19:45:04 +0300

    add file

---
+++ PYTHON/file.py
@@ -0,0 +1,25 @@
+# Lists
+# import numbers
+
+
+name = "joel"
+numbers = [2,3,4,5]
+fruits = ["Mango", "Oranges", "Papai", "PineApple"]
+print("mercy")
+print(numbers)
+print(fruits)
+print(len(fruits))
+fruits.append("Papai")
+
+# remove a string index in a list
+fruits.remove("Mango")
+fruits.insert(2, "Pineapple")
+fruits.insert(1, "Pineapple")
+fruits.reverse()
+fruits.sort()
+fruits[0] = "Potatos"
+
+print(fruits)
+
+
+
